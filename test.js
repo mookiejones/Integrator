baacd30eabff0f873cfc74a137e1b5988130f3d7
Charles Berman <Charles.Berman@kuka.co>
2017-06-26 08:57:46 -0400

    cleaning items out

--- test.js
+++ test.js
@@ -1,24 +1,32 @@
-var api=require('./src/KukaSql')
-var test=require('assert')
-var ContNum=2;
-var CAppNum=2;
+const test=require('assert');
+const fpt=require('./src/node/FanucProcess/test')
 
-console.log('test me');
-// api.getControllerNames(function(result){
-//     debugger;
-// })
 
-api.getControllerNames()
-    .then(result =>{
-        test.equal(result.length,14);
-    });
 
-api.getApplicationNames(ContNum)
-    .then(result=>{
-   //     debugger;
-    })
 
-api.getAppPaths(CAppNum)
-    .then(result=>{
-        debugger;
-    })
+test.equal('failed','failed')
+
+// var api=require('./src/KukaSql')
+// var test=require('assert')
+// var ContNum=2;
+// var CAppNum=2;
+
+// console.log('test me');
+// // api.getControllerNames(function(result){
+// //     debugger;
+// // })
+
+// api.getControllerNames()
+//     .then(result =>{
+//         test.equal(result.length,14);
+//     });
+
+// api.getApplicationNames(ContNum)
+//     .then(result=>{
+//    //     debugger;
+//     })
+
+// api.getAppPaths(CAppNum)
+//     .then(result=>{
+//         debugger;
+//     })
